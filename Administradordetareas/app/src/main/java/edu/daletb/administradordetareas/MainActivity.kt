0193583c99b9b55b6aff85ab22f22e5854dda155
dmsheyka <dmsheyka@gmail.com>
2024-03-07 11:29:57 -0400

    Proyecto terminado

--- Administradordetareas/app/src/main/java/edu/daletb/administradordetareas/MainActivity.kt
+++ Administradordetareas/app/src/main/java/edu/daletb/administradordetareas/MainActivity.kt
@@ -3,13 +3,25 @@ package edu.daletb.administradordetareas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.daletb.administradordetareas.ui.theme.AdministradorDeTareasTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AdminTarea()
                 }
             }
         }
@@ -30,17 +42,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AdminTarea(){
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ){
+        val img = painterResource(R.drawable.ic_task_completed)
+        Image(
+            painter = img,
+            contentDescription = null
+        )
+
+        Text(
+            text = stringResource(R.string.texto_tarea_completada),
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.texto_buen_trabajo),
+            fontSize = 16.sp
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun VistaPreviaTareas() {
     AdministradorDeTareasTheme {
-        Greeting("Android")
+        AdminTarea()
     }
 }
